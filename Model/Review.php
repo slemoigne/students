d7d3c1ee3877c3eecbdfd2d8c23b7ea22a2b76fd
slemoigne <sylvain.lemoigne@hotmail.fr>
2018-03-03 17:23:24 +0100

    Add Models Overwrite "recursive" of AppModel for more performance

--- Model/Review.php
+++ Model/Review.php
@@ -1,5 +1,41 @@
 <?php
 
+App::uses('AppModel', 'Model');
+
+/**
+ * Review Model
+ * 
+ */
 class Review extends AppModel {
+
+	/**
+	 *
+	 * @var array $validate
+	 */
+	public $validate = array(
+		'subject' => array(
+			'rule' => array('maxLength', 50),
+			'required' => true,
+			'allowEmpty' => false,
+			'message' => 'Subject is required (max length 50 characters).'
+		),
+		'grade' => array(
+			'rule' => array('range', -1, 21), // Limits values are exclude
+			'required' => true,
+			'allowEmpty' => false,
+			'message' => 'Grade is required and must be between 0 and 20 (include).'
+		)
+	);
+
+	/**
+	 *
+	 * @var array $belongsTo
+	 */
+	public $belongsTo = array(
+		'Student' => array(
+			'className' => 'Student',
+			'counterCache' => true // Increment and decrement review_count of student
+		)
+	);
 
 }
--- Model/Student.php
+++ Model/Student.php
@@ -1,5 +1,48 @@
 <?php
 
+App::uses('AppModel', 'Model');
+
+/**
+ * Student Model
+ * 
+ */
 class Student extends AppModel {
+
+	/**
+	 * 
+	 * @var array $validate
+	 */
+	public $validate = array(
+		'last_name' => array(
+			'rule' => array('maxLength', 50),
+			'required' => true,
+			'allowEmpty' => false,
+			'message' => 'Last name is required (max length 50 characters).'
+		),
+		'first_name' => array(
+			'rule' => array('maxLength', 50),
+			'required' => true,
+			'allowEmpty' => false,
+			'message' => 'First name is required (max length 50 characters).'
+		),
+		'birth_date' => array(
+			'rule' => array('date'),
+			'required' => true,
+			'allowEmpty' => false,
+			'message' => 'Date is not valid.'
+		)
+	);
+
+	/**
+	 * 
+	 * @var array $hasMany
+	 */
+	public $hasMany = array(
+		'Review' => array(
+			'className' => 'Review',
+			'dependent' => true, // Delete cascade
+			'exclusive' => true // One request to delete all reviews for more performance
+		)
+	);
 
 }
